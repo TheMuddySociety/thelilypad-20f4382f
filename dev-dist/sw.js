5989c541957dd982b1a986bd7d8dff4fa63663a2
The Muddy Society <110752952+TheMuddySociety@users.noreply.github.com>
2026-03-10 03:10:03 -0700

    feat: Implement service worker with Workbox for asset caching and offline functionality.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-b9cb0b6d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.arfg4tisje8"
+    "revision": "0.ilpu320g79c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
